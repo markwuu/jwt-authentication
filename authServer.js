33d204950f5df5f828e2f89a1de968b3e5c3c687
Mark W <markwuu@gmail.com>
2020-04-30 14:34:04 -0700

    Added notes

--- authServer.js
+++ authServer.js
@@ -8,11 +8,14 @@ app.use(express.json());
 // store this in a database
 let refreshTokens = [];
 
+//send the [refresh token] and receive a new [access token] using [user + ACCESS_TOKEN_SECRET]
 app.post('/token', (req, res) => {
     const refreshToken = req.body.token;
     if(refreshToken == null) return res.sendStatus(401);
+    //check if received [refresh token] is stored in our database
     if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
-    //verify refresh token, then create a new access token for user
+    //verify refresh token with REFRESH_TOKEN_SECRET
+    //then create a new [access token] for user
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
         const accessToken = generateAccessToken({ name: user.name });
@@ -20,23 +23,29 @@ app.post('/token', (req, res) => {
     });
 });
 
+//send the [refresh token] and then remove it from the database
 app.delete('/logout', (req, res) => {
     refreshTokens = refreshTokens.filter(token => token !== req.body.token);
     res.sendStatus(204);
 });
 
+//login with username and receive an [access token] and [refresh token]
 app.post('/login', (req, res) => {
     // Authenticate user
 
     const username = req.body.username;
     const user = { name: username };
 
+    //generates access token using username (expires in x)
     const accessToken = generateAccessToken(user);
+    //generates refresh token using username (no expiration)
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    //store this refresh token in your database
     refreshTokens.push(refreshToken);
     res.json({ accessToken, refreshToken });
 });
 
+//generates an [access token] using [user + ACCESS_TOKEN_SECRET]
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '25s'});
 }
--- server.js
+++ server.js
@@ -16,24 +16,23 @@ const posts = [
     }
 ]
 
-//GET /posts sends along access token
-//authenticateToken will verify the token in the GET request is the same by running it through a function that takes the token in the GET request and compares it to the ACCESS_TOKEN_SECRET in the .env file
+//GET /posts sends along access token in the header
 app.get('/posts', authenticateToken, (req, res) => {
     // return list of posts where the post.username is equal to the name on the get request
     res.json(posts.filter(post => post.username === req.user.name));
 });
 
+//authenticateToken will verify the token in the GET request
+//is generated from the ACCESS_TOKEN_SECRET using the verify function
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (token === null) return res.sendStatus(401);
 
-    //verify token that user was sent during login. If verified, allow next middleware to fire
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) return res.sendStatus(403);
-        //if verified, we set the user to the req.user in the next middleware
-        req.user = user;
-        next();
+        req.user = user; //pass user into request object
+        next(); //and allow next callback to fire
     });
 };
 
